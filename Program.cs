039df64bc5d85da763e3c460bf232c47bf89e7f8
AngelaRebancosBautista <angelarbautista22@gmail.com>
2025-07-25 12:50:39 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory_Activity_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            string input;
+
+            while (true)
+            {
+                Console.Write("Enter a number (or type 'stop' to finish): ");
+                input = Console.ReadLine();
+
+                if (input.Equals("stop", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+                if (int.TryParse(input, out int number))
+                {
+                    sum += number; 
+                }
+                else
+                {
+                    Console.WriteLine("Invalid entry, please enter a valid number.");
+                }
+            }
+            Console.WriteLine("Sum of all valid numbers: " + sum);
+        }
+    }
+}
+           
